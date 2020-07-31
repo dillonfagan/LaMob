6810412d0b52dc701ebc161b365ec337aa1d1de7
dfagan <dfagan@emoneyadvisor.com>
2020-07-31 13:49:06 -0400

    Add button that doesn't work

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+	import Timer from './Timer.svelte';
+	export let name;
+</script>
+
+<main>
+	<h1>La Mob</h1>
+	<Timer />
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+		background-color: #218c74;
+	}
+
+	h1 {
+		color: #ffffff;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/Timer.svelte
@@ -0,0 +1,28 @@
+<script>
+	const minutesToSeconds = (minutes) => minutes * 60;
+	const secondsToMinutes = (seconds) => Math.floor(seconds / 60);
+	const padWithZeroes = (number) => number.toString().padStart(2, '0');
+
+	const lengthInSeconds = minutesToSeconds(1);
+
+	let time = lengthInSeconds;
+
+	function start() {}
+
+	function formatTime(timeInSeconds) {
+		const minutes = secondsToMinutes(timeInSeconds);
+		const remainingSeconds = timeInSeconds % 60;
+		return `${padWithZeroes(minutes)}:${padWithZeroes(remainingSeconds)}`;
+	}
+</script>
+
+<section>
+	<p class="time">{formatTime(time)}</p>
+	<button on:click={start}>Start</button>
+</section>
+
+<style>
+	.time {
+		color:white;
+	}
+</style>
