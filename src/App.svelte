85138a2f37a67f5a3444c36cf7b95a991e862dc2
Dillon Fagan <dillonfagan@pm.me>
2020-08-06 16:30:53 -0400

    Add tailwindcss framework

--- src/App.svelte
+++ src/App.svelte
@@ -7,20 +7,8 @@
 	<Timer />
 </main>
 
-<style>
-	main {
-		text-align: center;
-		background-color: #218c74;
-		display: block;
-		height: 98vh;
-		padding-top: 2vh;
-	}
-
-	h1 {
-		color: #ffffff;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-		margin-top: 0;
-	}
+<style global>
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
 </style>
