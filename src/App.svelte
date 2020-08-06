a2b7f22d9247b7a8b7f15996631cdf68eedf1b35
Dillon Fagan <dillonfagan@pm.me>
2020-08-06 16:37:22 -0400

    Style App component

--- src/App.svelte
+++ src/App.svelte
@@ -2,8 +2,8 @@
 	import Timer from './Timer.svelte';
 </script>
 
-<main id="main">
-	<h1>La Mob</h1>
+<main id="main" class="m-auto w-full h-full p-16 bg-green-600">
+	<h1 class="text-6xl text-white">La Mob</h1>
 	<Timer />
 </main>
 
