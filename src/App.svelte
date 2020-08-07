a08ba4e4c9435483f2290ac1fc17fefd9e7b53ca
dfagan <dfagan@emoneyadvisor.com>
2020-08-07 15:32:32 -0400

    Background change with state

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,9 @@
 <script>
 	import Timer from './Timer.svelte';
+	import { running } from './store';
 </script>
 
-<main id="main" class="w-full h-full p-16 bg-green-600 flex items-center justify-center">
+<main id="main" class="w-full h-full p-16 {$running ? "bg-purple-700" : "bg-green-600"} flex items-center justify-center">
 	<div class="mw-lg">
 		<h1 class="text-6xl text-white text-center uppercase italic">La Mob</h1>
 		<Timer />
--- src/Timer.svelte
+++ src/Timer.svelte
@@ -1,11 +1,12 @@
 <script>
+	import { running } from './store.js';
+
 	const minutesToSeconds = (minutes) => minutes * 60;
 	const secondsToMinutes = (seconds) => Math.floor(seconds / 60);
 	const padWithZeroes = (number) => number.toString().padStart(2, '0');
 
 	let time;
 	let interval;
-	let running = false;
 
 	initializeTimer();
 
@@ -14,15 +15,8 @@
 		time = lengthInSeconds;
 	}
 
-	function setBackgroundColor(color) {
-		document.getElementById("main")
-		.style
-		.backgroundColor = color;
-	}
-
 	function start() {
-		running = true;
-		setBackgroundColor("#474787");
+		running.set(true);
 		const timeInput = parseFloat(document.getElementById("time-input").value);
 		time = minutesToSeconds(timeInput);
 
@@ -37,11 +31,10 @@
 
 	function stopTimer(interval) {
 		clearInterval(interval);
-		setBackgroundColor("#b33939");
 	}
 
 	function reset() {
-		location.reload();
+		running.set(false);
 	}
 
 	function formatTime(timeInSeconds) {
@@ -54,5 +47,5 @@
 <section>
 	<div class="text-3xl text-white w-full text-center">{formatTime(time)}</div>
 	<input id="time-input" value="0.1" class="py-2 px-4 text-xl text-white bg-transparent border-b-2" />
-	<button id="button" on:click={running ? reset : start} class="bg-white py-2 px-6 mt-4 rounded-full">{running ? `Reset` : `Start`}</button>
+	<button id="button" on:click={$running ? reset : start} class="bg-white py-2 px-6 mt-4 rounded-full">{$running ? `Reset` : `Start`}</button>
 </section>
