b846e2f2535b40e8b94e2f356564a9c5419a98fe
Dillon Fagan <dillonfagan@pm.me>
2020-08-09 13:30:05 -0400

    Reset automatically and hide fields when running

--- src/Timer.svelte
+++ src/Timer.svelte
@@ -13,19 +13,28 @@
 		interval = setInterval(() => {
 			if (time === 0) {
 				clearInterval(interval);
+				running.set(false);
 				return;
     		}
     		time -= 1;
 		},1000);
 	}
-
-	function reset() {
-		running.set(false);
-	}
 </script>
 
 <section>
-	<div class="text-3xl text-white w-full text-center">{Time.format(time)}</div>
-	<input bind:value={rawTimeInput} class="py-2 px-4 text-xl text-white bg-transparent border-b-2" />
-	<button on:click={$running ? reset : start} class="bg-white py-2 px-6 rounded-full">{$running ? `Reset` : `Start`}</button>
+	<div class="text-3xl text-white w-full text-center" class:big={$running}>{Time.format(time)}</div>
+	<div class:hidden={$running}>
+		<input bind:value={rawTimeInput} class="py-2 px-4 text-xl text-white bg-transparent border-b-2" />
+		<button on:click={start} class="bg-white py-2 px-6 rounded-full">Start</button>
+	</div>
 </section>
+
+<style>
+	.hidden {
+		@apply hidden;
+	}
+
+	.big {
+		@apply text-5xl;
+	}
+</style>
