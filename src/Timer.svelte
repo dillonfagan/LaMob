c107df304ffb2b2fb7b6038a63c139c8b45bf526
Dillon Fagan <dillonfagan@pm.me>
2020-08-09 13:31:11 -0400

    Hide time when not running

--- src/Timer.svelte
+++ src/Timer.svelte
@@ -22,7 +22,7 @@
 </script>
 
 <section>
-	<div class="text-3xl text-white w-full text-center" class:big={$running}>{Time.format(time)}</div>
+	<div class="text-3xl text-white w-full text-center" class:big={$running} class:hidden={!$running}>{Time.format(time)}</div>
 	<div class:hidden={$running}>
 		<input bind:value={rawTimeInput} class="py-2 px-4 text-xl text-white bg-transparent border-b-2" />
 		<button on:click={start} class="bg-white py-2 px-6 rounded-full">Start</button>
