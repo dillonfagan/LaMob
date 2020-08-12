8a2fe12cfa6eb87628dc92e9101ad97689b1246e
Dillon Fagan <dillonfagan@pm.me>
2020-08-12 18:05:27 -0400

    Add turn transitions

--- src/Timer.svelte
+++ src/Timer.svelte
@@ -7,24 +7,37 @@
 	let time;
 	let interval;
 	
+	let waiting = false;
 	let mobberIndex = 0;
 	let mobber;
+	let nextMobber;
 
-	function start() {
-		running.set(true);
-		mobbers.set(listMobbers());
-		mobber = $mobbers[mobberIndex];
-
+	function next() {
+		waiting = false;
 		time = Time.minutesToSeconds(rawTimeInput);
+		mobber = $mobbers[mobberIndex];
 
 		interval = setInterval(() => {
 			if (time === 0) {
 				clearInterval(interval);
-				running.set(false);
+				if (mobberIndex < $mobbers.length - 1) {
+					mobberIndex++;
+					nextMobber = $mobbers[mobberIndex];
+					waiting = true;
+				}
+				else {
+					running.set(false);
+				}
 				return;
     		}
     		time -= 1;
 		},1000);
+	}
+
+	function start() {
+		mobbers.set(listMobbers());
+		running.set(true);
+		next();
 	}
 
 	function listMobbers() {
@@ -38,7 +51,16 @@
 
 <section>
 	<div class="text-5xl text-white w-full text-center" class:hidden={!$running}>{Time.format(time)}</div>
-	<div class="text-3xl text-purple-900 w-full text-center" class:hidden={!$running}>{mobber}</div>
+	<div class="text-3xl text-purple-900 w-full text-center" class:hidden={(!$running || waiting)}>{mobber}</div>
+	<div class="w-full flex overflow-hidden py-2 px-4 bg-purple-600 border-2 border-purple-900 rounded-full" class:hidden={!waiting}>
+		<div class="text-xl text-white">{mobber}</div>
+		<button on:click={next} class="text-purple-900 hover:text-white mx-2 rounded-full">
+			<svg class="fill-current h-8 w-8" viewBox="0 0 20 20">
+				<path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z" clip-rule="evenodd" />
+			</svg>
+		</button>
+		<div class="text-xl text-white">{nextMobber}</div>
+	</div>
 
 	<div class="flex overflow-hidden py-2 px-4 mb-4 bg-green-500 border-2 border-green-800 rounded-full" class:hidden={$running}>
 		<input type="text" placeholder="Mobbers" bind:value={rawMobbersInput} class="text-xl text-white placeholder-green-800 bg-transparent outline-none" />
