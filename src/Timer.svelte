c87a9363b663b0870fa55186ca47a34d0db03d77
dfagan <dfagan@emoneyadvisor.com>
2020-08-14 15:13:00 -0400

    Remove button id

--- src/Timer.svelte
+++ src/Timer.svelte
@@ -26,7 +26,7 @@
 	<div class="text-6xl text-white w-full text-center" class:hidden={!$running}>{formatTime(time)}</div>
 	<div class:hidden={$running}>
 		<input bind:value={timeInput} class="py-2 px-4 text-xl text-white bg-transparent border-b-2" />
-		<button id="button" on:click={start} class="bg-white py-2 px-6 mt-4 rounded-full">Start</button>
+		<button on:click={start} class="bg-white py-2 px-6 mt-4 rounded-full">Start</button>
 	</div>
 	<div class="w-full flex justify-center" class:hidden={!$waiting}>
 		<button class="bg-white py-2 px-6 mt-4 rounded-full" on:click={start}>Next</button>
