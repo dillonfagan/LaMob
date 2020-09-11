faba82fd00cf7f7ec32ce3dbb1281640702d5d9b
dfagan <dfagan@emoneyadvisor.com>
2020-09-11 13:35:36 -0400

    Play music when timer stops

--- src/TimerView.svelte
+++ src/TimerView.svelte
@@ -2,10 +2,12 @@
 	import { onDestroy } from 'svelte';
 	import { minutesToSeconds, formatTime } from './timer';
 	import State from './state';
+	import Music from './music.js';
 
 	let time;
 	let interval = setInterval(() => {
 		if (time === 0) {
+			Music.play();
 			clearInterval(interval);
 			return;
 		}
@@ -18,5 +20,9 @@
 
 	onDestroy(unsubscribe);
 </script>
+
+<svelte:body
+	on:mouseenter={Music.stop}
+/>
 
 <div class="text-6xl text-white w-full text-center">{formatTime(time)}</div>
