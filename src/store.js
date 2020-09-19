c6e0964e7e0f1984452b37a9a5e258ca84c3115a
Dillon Fagan <dillonfagan@pm.me>
2020-09-19 10:28:39 -0400

    Remove store.js

--- src/store.js
+++
@@ -1,6 +0,0 @@
-import { writable } from 'svelte/store';
-
-export let running = writable(false);
-export let waiting = writable(false);
-export let mobbers = writable([]);
-export let currentMobber = writable('La Mob');
