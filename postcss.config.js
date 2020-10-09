c58cad9973e28e09a87a008dd0abb6eecbcf3b01
dfagan <dfagan@emoneyadvisor.com>
2020-10-09 14:31:28 -0400

    Electron works

--- postcss.config.js
+++ postcss.config.js
@@ -3,9 +3,8 @@ const purgecss = require('@fullhuman/postcss-purgecss') ({
     whitelistPatterns: [/svelte-/],
     defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
 });
-  
+
 module.exports = {
-    plugins: [require('tailwindcss'), 
-    ...(!process.env.ROLLUP_WATCH ? [purgecss] : [])],
+    plugins: [require('tailwindcss'),]
+    // ...(!process.env.ROLLUP_WATCH ? [purgecss] : [])],
 };
-  
