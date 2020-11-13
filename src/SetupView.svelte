0ec52eeabb77c374c8f4452b85e82dc3813292e6
dfagan <dfagan@emoneyadvisor.com>
2020-11-13 14:12:53 -0500

    Add shuffle icon to SetupView

--- src/SetupView.svelte
+++ src/SetupView.svelte
@@ -30,11 +30,20 @@
 
 <h1 class="text-3xl text-white text-center uppercase italic">La Mob</h1>
 <div class="flex flex-col justify-center">
-	<input
-		placeholder="Les Mobbeurs"
-		bind:value={mobbersInput}
-		class="py-2 px-4 text-xl text-white  placeholder-green-900 bg-transparent border-b-2"
-	/>
+	<div class="flex flex-row border-b-2">
+		<input
+			placeholder="Les Mobbeurs"
+			bind:value={mobbersInput}
+			class="py-2 px-4 text-xl text-white placeholder-green-900 bg-transparent"
+		/>
+		<svg xmlns="http://www.w3.org/2000/svg" class="text-green-900 cursor-pointer" width="36" height="32" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+			<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+			<path d="M3 17h2.397a5 5 0 0 0 4.096 -2.133l.177 -.253m3.66 -5.227l.177 -.254a5 5 0 0 1 4.096 -2.133h3.397" />
+			<path d="M18 4l3 3l-3 3" />
+			<path d="M3 7h2.397a5 5 0 0 1 4.096 2.133l4.014 5.734a5 5 0 0 0 4.096 2.133h3.397" />
+			<path d="M18 20l3 -3l-3 -3" />
+		</svg>
+	</div>
 	<input
 		bind:this={timeField}
 		value="7"
